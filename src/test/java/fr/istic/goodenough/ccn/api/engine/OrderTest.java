159a9504c914c1ba494909390aff5388291cb460
Benjamin Navet <benjamin.navet@live.fr>
2021-03-26 14:39:41 +0100

    order tests2

--- src/test/java/fr/istic/goodenough/ccn/api/engine/OrderTest.java
+++ src/test/java/fr/istic/goodenough/ccn/api/engine/OrderTest.java
@@ -1,7 +1,12 @@
 package fr.istic.goodenough.ccn.api.engine;
 
+import fr.istic.goodenough.ccn.api.data.PhonyData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.validation.constraints.AssertTrue;
+import java.util.Collection;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,12 +20,16 @@ class OrderTest {
 
     @BeforeEach
     void setUp() {
+        cust = new CustomerImpl("Jean Hubert", "creponutella", 1002);
+        prod = new ProductImpl(1004, "Jambon fromage", "Jam/From", 10.0, -1, "nourriture");
+        order = new OrderImpl(prod,cust,1);
     }
 
     /** Try to get the customer from an order
      * Asserts that the customer is equal to the Order's costumer */
     @Test
     void getCustomer() {
+        assertEquals(cust, order.getCustomer(), "The customer should be the same as Jean Hubert");
     }
 
     @Test
@@ -43,6 +52,19 @@ class OrderTest {
         assertEquals(rep, order.getAmount());
     }
 
+    /** Try to set the amount of an order
+     * Asserts that the amount is well-set
+     * test with a product stock limited to 5 */
+    @ParameterizedTest(name = "Limited stock : Amount is {0} and answer is {1}")
+    @CsvSource({"-1,1","0,0","2,2","6,1"})
+    void setAmountLimitedStock(int amount, int rep){
+        Product prod2 = new ProductImpl(1004, "Jambon fromage", "Jam/From", 10.0, 5, "nourriture");
+        Order order2 = new OrderImpl(prod2,cust,1);
+        assertEquals(amount==rep,order2.setAmount(amount));
+        assertEquals(rep, order2.getAmount());
+    }
+
+    /** test the getter for order's price */
     @Test
     @DisplayName("Order's price must match amount*price")
     void getOrderPrice() {
--- src/test/java/fr/istic/goodenough/ccn/api/engine/ProductTest.java
+++ src/test/java/fr/istic/goodenough/ccn/api/engine/ProductTest.java
@@ -7,32 +7,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
+    private Product product;
+
     @BeforeEach
     void setUp() {
+        product = new ProductImpl(1004, "Jambon fromage", "Jam/From", 10.0, -1, "nourriture");
     }
 
+    /** Try to get the pid of a product
+     * Asserts that the pid is equal to 1004 */
     @Test
     void getPid() {
+        assertEquals(1004, product.getPid(), "The pid should be the same as 1004");
     }
 
+    /** Try to get the full name of a product
+     * Asserts that the full name is equal to "Jambon fromage" */
     @Test
     void getFullName() {
+        assertEquals("Jambon fromage", product.getFullName(), "The full name should be the same as \"Jambon fromage\"");
     }
 
+    /** Try to get the short name of a product
+     * Asserts that the short name is equal to "Jam/From" */
     @Test
     void getShortName() {
+        assertEquals("Jam/From", product.getShortName(), "The short name should be the same as \"Jam/From\"");
     }
 
+    /** Try to get the price of a product
+     * Asserts that the price is equal to 10.0 */
     @Test
     void getPrice() {
+        assertEquals(10.0, product.getPrice(), "The price should be the same as 10.0");
     }
 
+    /** Try to get the stock of a product
+     * Asserts that the stock is equal to -1 (unlimited stock) */
     @Test
     void getStock() {
+        assertEquals(-1, product.getStock(), "The stock should be the same as -1");
     }
 
+    /** Try to get the type of a product
+     * Asserts that the type is equal to "nourriture" */
     @Test
     void getType() {
+        assertEquals("nourriture", product.getType(), "The type should be the same as \"nourriture\"");
     }
 
     @Test
