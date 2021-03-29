2b556603250b4cd7788cf200350bf36f0df3271f
ggcow <ggcow@mail.com>
2021-03-29 17:26:00 +0200

    Ajoute constructeur par défaut à ProductDTO et transforme JSON en liste de ProductDTO

--- src/test/java/fr/istic/goodenough/ccn/api/rest/Login.java
+++ src/test/java/fr/istic/goodenough/ccn/api/rest/Login.java
@@ -3,11 +3,15 @@ package fr.istic.goodenough.ccn.api.rest;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
 // JUnit 4 Test helper
 import org.junit.Test;
 // JUnit 5 Assertions (!!!)
 import org.junit.jupiter.api.*;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Login extends JerseyTest {
@@ -19,8 +23,12 @@ public class Login extends JerseyTest {
 
     @Test
     public void test() {
-        Response response = target("/products").queryParam("uid", 1).request().get(Response.class);
-        assertEquals(200,response.getStatus());
-        System.out.println("réponse : " + response.readEntity(String.class));
+        List<ProductDTO> products = target("/products")
+                .queryParam("uid", 1)
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<ProductDTO>>(){});
+        for (ProductDTO p : products) {
+            System.out.println(p.pid+" "+p.name+" ("+p.type+") costs "+p.price);
+        }
     }
 }
