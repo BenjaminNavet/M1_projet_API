b7487f048d8384ef5aa5f03f13bfd8c0b8fa3cf3
Boris Sachet <boris.sachet@gmail.com>
2021-03-29 10:14:29 +0200

    Modified order objects declaration after interface change

--- src/test/java/fr/istic/goodenough/ccn/api/engine/OrderTest.java
+++ src/test/java/fr/istic/goodenough/ccn/api/engine/OrderTest.java
@@ -21,9 +21,8 @@ class OrderTest {
     void setUp() {
         cust = new CustomerImpl("Jean Hubert", "creponutella", 1002);
         prod = new ProductImpl(1004, "Jambon fromage", "Jam/From", 10.0, -1, "nourriture");
-        order = new OrderImpl(prod,cust,1);
-        //order = new OrderImpl(prod,cust);
-        //order.setAmount(1);
+        order = new OrderImpl(prod,cust);
+        order.setAmount(1);
     }
 
     /** Try to get the customer from an order
@@ -81,9 +80,8 @@ class OrderTest {
     @Tag("UnitTest")
     void setAmountLimitedStockUnit(int amount, int rep){
         Product prod2 = new ProductImpl(1004, "Jambon fromage", "Jam/From", 10.0, 5, "nourriture");
-        Order order2 = new OrderImpl(prod2,cust,1);
-        //Order order2 = new OrderImpl(prod2,cust);
-        //order2.setAmount(1);
+        Order order2 = new OrderImpl(prod2,cust);
+        order2.setAmount(1);
         assertTrue(order2.setAmount(amount));
         assertEquals(rep, order2.getAmount());
     }
@@ -96,9 +94,8 @@ class OrderTest {
     @Tag("RobustnessTest")
     void setAmountLimitedStockRobustness(int amount, int rep){
         Product prod2 = new ProductImpl(1004, "Jambon fromage", "Jam/From", 10.0, 5, "nourriture");
-        Order order2 = new OrderImpl(prod2,cust,1);
-        //Order order2 = new OrderImpl(prod2,cust);
-        //order2.setAmount(1);
+        Order order2 = new OrderImpl(prod2,cust);
+        order2.setAmount(1);
         assertFalse(order2.setAmount(amount));
         assertEquals(rep, order2.getAmount());
     }
