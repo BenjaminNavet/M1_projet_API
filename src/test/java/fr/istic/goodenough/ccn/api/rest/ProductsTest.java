640ffe0dd8066c6a01440c8066feeee5796cb2de
ggcow <ggcow@mail.com>
2021-03-31 15:28:26 +0200

    Renamed Login to ProductsTest

--- src/test/java/fr/istic/goodenough/ccn/api/rest/ProductsTest.java
+++ src/test/java/fr/istic/goodenough/ccn/api/rest/ProductsTest.java
@@ -9,13 +9,13 @@ import javax.ws.rs.core.Response;
 // JUnit 4 Test helper
 import org.junit.Test;
 // JUnit 5 Assertions (!!!)
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-public class Login extends JerseyTest {
+
+public class ProductsTest extends JerseyTest {
 
     @Override
     protected Application configure() {
