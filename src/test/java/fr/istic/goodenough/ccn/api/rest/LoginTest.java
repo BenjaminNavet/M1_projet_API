789a610094a52f86c3f38314bbd344cdcda4fbb2
amdeniel <amandine.deniel@etudiant.univ-rennes1.fr>
2021-04-02 13:40:30 +0200

    Add testWrongCoupleLoginPassword and add test error message for testWrongPassword

--- src/test/java/fr/istic/goodenough/ccn/api/rest/LoginTest.java
+++ src/test/java/fr/istic/goodenough/ccn/api/rest/LoginTest.java
@@ -73,7 +73,8 @@ public class LoginTest extends JerseyTest {
      *  Init :
      *  1- Build and execute request.
      *  Expected :
-     *  1- Http response code is 404 / http_not_found */
+     *  1- Http response code is 404 / http_not_found
+     *  2- Json message contains "Customer not found / Invalid credentials" */
     @Test
     @DisplayName("Wrong password")
     public void testWrongPassword(){
@@ -81,6 +82,27 @@ public class LoginTest extends JerseyTest {
         Response response= target("/login")
                 .queryParam("name","nemo")
                 .queryParam("passwd","login")
+                .request(MediaType.APPLICATION_JSON)
+                .get(Response.class);
+        // Assert HTTP_Notfound
+        assertEquals(404, response.getStatus());
+        String body = response.readEntity(String.class);
+        assertTrue(body.contains("Customer not found / Invalid credentials"));
+    }
+
+    /** Test if the request of a connection fail with a wrong couple login/password witch are both good for two different
+     *  customers by returning a 404 response code
+     *  Init :
+     *  1- Build and execute request.
+     *  Expected :
+     *  1- Http response code is 404 / http_not_found */
+    @Test
+    @DisplayName("Wrong couple login password")
+    public void testWrongCoupleLoginPassword(){
+        // Build and execute request
+        Response response= target("/login")
+                .queryParam("name","Emma")
+                .queryParam("passwd","0000")
                 .request(MediaType.APPLICATION_JSON)
                 .get(Response.class);
         // Assert HTTP_Notfound
