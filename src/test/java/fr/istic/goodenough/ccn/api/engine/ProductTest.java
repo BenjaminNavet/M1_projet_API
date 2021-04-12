27446a99767a6b628798af3b5b71c999d883f35d
Benjamin Navet <benjamin.navet@live.fr>
2021-04-12 17:44:47 +0200

    Modif doc Product test dans l'engine

--- src/test/java/fr/istic/goodenough/ccn/api/engine/ProductTest.java
+++ src/test/java/fr/istic/goodenough/ccn/api/engine/ProductTest.java
@@ -19,18 +19,26 @@ class ProductTest {
         product2 = new ProductImpl(1004, "Jambon fromage", "Jam/From", 10.0, 5, "nourriture");
     }
 
-    /** Try to get the pid of a product
-     * Asserts that the pid is equal to 1004 */
+    /** Tests if the pid returned by the getPid method() is the same as initiated in setup: 1004
+     *  Init: nothing more than setup
+     *  Expected: returned pid is the same as 1004
+     *  */
     @Test
     @Tag("UnitTest")
     void getPid() {
         assertEquals(1004, product.getPid(), "The pid should be the same as 1004");
     }
 
-    /** Try to get the full name of a product
-     * Asserts that the full name is equal to "Jambon fromage" */
+
+
+    /** Tests if the full name of a product returned by the getFullName method() is the same as
+     * initiated in the setup : Jambon Fromage
+     *  Init: nothing more than setup
+     *  Expected: return full name of product as "Jambon Fromage"
+     *  */
     @Test
     @Tag("UnitTest")
+    @DisplayName("Return the Full name conform to the product setup")
     void getFullName() {
         assertEquals("Jambon fromage", product.getFullName(), "The full name should be the same as \"Jambon fromage\"");
     }
@@ -51,10 +59,14 @@ class ProductTest {
         assertEquals(10.0, product.getPrice(), "The price should be the same as 10.0");
     }
 
-    /** Try to get the stock of a product
-     * Asserts that the stock is equal to -1 (unlimited stock) */
+    /** Test if the stock of the product returned by getStock method() is the same as initiated setup: -1
+     * which is infinite stock
+     * init : nothing more than setup
+     * Expected : return the stock of the product as -1 (unlimited stock)
+     * */
     @Test
     @Tag("UnitTest")
+    @DisplayName("Return the stock conform to the product setup")
     void getStock() {
         assertEquals(-1, product.getStock(), "The stock should be the same as -1");
     }
@@ -67,13 +79,20 @@ class ProductTest {
         assertEquals("nourriture", product.getType(), "The type should be the same as \"nourriture\"");
     }
 
-    /** Try to take products from unlimited stock
-     * Assert that stock hasn't been changed
+    /** Test if the takeFromStock method() doesn't change stock value of a product when this one
+     * is initiated at -1 (unlimited)
+     * Init:
+     *  - set quantity to take from stock at 0
+     *  - set quantity to take from stock at 2
+     *  - set quantity to take from stock at 800000
+     * Expected: stock doesn't change
+     *  - Stock quantity is -1 (x3)
      * @param amount int Product's amount */
     @ParameterizedTest(name = "Amount is {0} and newStock=oldStock")
     @CsvSource({"0","2","800000"})
     @Tag("UnitTest")
-    void takeFromWithUnlimitedStockUnit(int amount) {
+    @DisplayName("Test the stock amount when initiated unlimited and quantity is taken from it")
+    void takeFromStockWithUnlimitedStockUnit(int amount) {
         int oldStock=product.getStock();
         assertTrue(product.takeFromStock(amount));
         int newStock=product.getStock();
@@ -93,12 +112,22 @@ class ProductTest {
         assertEquals(oldStock,newStock);
     }
 
-    /** Try to take products from limited stock
-     * Assert that stock has been changed
+
+    /**Test if the takeFromStock method() does correctly change stock value
+     * of a product when this one is limited at 5 and user try to take quantity from stock
+     * Init:
+     *  - set quantity to take from stock at 0
+     *  - set quantity to take from stock at 2
+     *  - set quantity to take from stock at 5
+     * Expected: stock change as we take quantity from it
+     *  - Stock quantity is 5
+     *  - Stock quantity is 3
+     *  - Stock quantity is 0
      * @param amount int Product's amount */
     @ParameterizedTest(name = "Amount is {0} and newStock=oldStock-Amount")
     @CsvSource({"0","2","5"})
     @Tag("UnitTest")
+    @DisplayName("Test the stock amount when limited and quantity is taken from it")
     void takeFromStockWithLimitedStockUnit(int amount) {
         int oldStock = product2.getStock();
         assertTrue(product2.takeFromStock(amount));
@@ -132,8 +161,15 @@ class ProductTest {
         assertEquals(oldStock,newStock);
     }
 
-    /** Try to put products in limited stock
-     * Assert that stock has been changed
+    /** Test if the putInStock() method change the stock correctly when we ask to put 0, 2 and 5 product(s) in limited stock
+     * Init:
+     * - set quantity to put in stock at 0
+     * - set quantity to put in stock at 2
+     * - set quantity to put in stock at 5
+     * Expected : the new stock is equal to the old stock + the quantity (added)
+     * - Stock quantity is 5
+     * - Stock quantity is 7
+     * - Stock quantity is 10
      * @param amount int Product's amount */
     @ParameterizedTest(name = "Amount is {0} and newStock=oldStock+Amount")
     @CsvSource({"0","2","5"})
@@ -145,12 +181,17 @@ class ProductTest {
         assertEquals(oldStock+amount,newStock);
     }
 
-    /** Try to put outlier amount to limited stock and unlimited stock
-     * Assert that stock hasn't been changed
+
+     /** Test if the putInStock method() does not change the stock value of unlimited stock and limited stock
+       when parametized with incorrect value -1.
+     * Init:
+     *  - set quantity to take from stock at -1
+     * Expected: the old stock and the newStock are equals (because it's an unlimited stock)
      * @param amount int Product's amount */
     @ParameterizedTest(name = "Amount is {0} but is bad value")
     @CsvSource({"-1"})
     @Tag("RobustnessTest")
+    @DisplayName("Test to putInStock() on limited stock and unlimited stock : try to put -1 quantity")
     void putInStockRobustnessTest(int amount) {
         int oldStock=product2.getStock();
         assertFalse(product2.putInStock(amount));
